be69ac405196a3ad072a45da3c8e53c8cb56aec0
DeJuan Anderson <dejuan@mit.edu>
2013-12-02 02:11:10 -0500

    I finished adding the palette I mentioned and cleaned up some code.

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -13,17 +13,32 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+
 
 /**
- * Canvas represents a drawing surface that allows the user to draw
- * on it freehand, with the mouse.
+ * This is my finalized implementation of the Warmup. 
+ * I added a toolbar and fully functional color palette.
+ * 
+ * Note that as it functions now, the palette popup bypasses the 
+ * palette listener, directly returning the color when the user clicks OK.
+ * I then change the current pen color to the returned color from the dialog.
+ * @author DeJuan 
  */
-public class Canvas extends JPanel {
-    // image where the user's drawing is stored
+public class Canvas extends JPanel{
+    
+	private static final long serialVersionUID = 1L;
+	// image where the user's drawing is stored
     private Image drawingBuffer;
+    private boolean eraserMode = false;
+    private JTextField strokeWidth;
+    private Color currentPenColor = Color.black;
+    private JColorChooser palette = new JColorChooser(Color.black);
+    
     
     
     /**
@@ -31,9 +46,39 @@ public class Canvas extends JPanel {
      * @param width width in pixels
      * @param height height in pixels
      */
-    public Canvas(int width, int height) {
+    public Canvas(int width, int height) 
+    {
         this.setPreferredSize(new Dimension(width, height));
         addDrawingController();
+        addEraserController();
+        
+        JToolBar toolbar = new JToolBar();
+       
+        
+        PaletteListener paletteController = new PaletteListener();
+        palette.getSelectionModel().addChangeListener(paletteController);
+        palette.setPreviewPanel(new JPanel());
+        
+        JButton paletteButton = new JButton("Show Color Palette");
+        paletteButton.addMouseListener(new PalettePopupListener());
+        
+        JButton resetter = new JButton("Reset All");
+        resetter.addMouseListener(new resetListener());
+        
+        JLabel widthLabel = new JLabel("Pen Size");
+        strokeWidth = new JTextField("1");
+        strokeWidth.setPreferredSize(new Dimension(40,20));
+        
+        JToggleButton DrawOrErase = new JToggleButton("Eraser Mode",false);
+        DrawOrErase.addMouseListener(new eraseButtonListener());
+        toolbar.add(DrawOrErase);
+        toolbar.add(paletteButton);
+        toolbar.add(widthLabel);
+        toolbar.add(strokeWidth);
+        toolbar.add(resetter);
+        add(toolbar);
+        //Make the buttons, add the mouse listener to them, and add the buttons to the canvas.
+        
         // note: we can't call makeDrawingBuffer here, because it only
         // works *after* this canvas has been added to a window.  Have to
         // wait until paintComponent() is first called.
@@ -43,10 +88,12 @@ public class Canvas extends JPanel {
      * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
      */
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) 
+    {
         // If this is the first time paintComponent() is being called,
         // make our drawing buffer.
-        if (drawingBuffer == null) {
+        if (drawingBuffer == null) 
+        {
             makeDrawingBuffer();
         }
         
@@ -57,7 +104,8 @@ public class Canvas extends JPanel {
     /*
      * Make the drawing buffer and draw some starting content for it.
      */
-    private void makeDrawingBuffer() {
+    private void makeDrawingBuffer() 
+    {
         drawingBuffer = createImage(getWidth(), getHeight());
         fillWithWhite();
         drawSmile();
@@ -66,7 +114,8 @@ public class Canvas extends JPanel {
     /*
      * Make the drawing buffer entirely white.
      */
-    private void fillWithWhite() {
+    private void fillWithWhite() 
+    {
         final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
 
         g.setColor(Color.WHITE);
@@ -80,7 +129,8 @@ public class Canvas extends JPanel {
     /*
      * Draw a happy smile on the drawing buffer.
      */
-    private void drawSmile() {
+    private void drawSmile() 
+    {
         final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
 
         // all positions and sizes below are in pixels
@@ -114,10 +164,19 @@ public class Canvas extends JPanel {
      * Draw a line between two points (x1, y1) and (x2, y2), specified in
      * pixels relative to the upper-left corner of the drawing buffer.
      */
-    private void drawLineSegment(int x1, int y1, int x2, int y2) {
+    private void drawLineSegment(int x1, int y1, int x2, int y2) 
+    {
         Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
-        
-        g.setColor(Color.BLACK);
+        g.setColor(currentPenColor);
+        String penSizeStr = strokeWidth.getText();
+        int penSize;
+        try{
+        	penSize = Integer.parseInt(penSizeStr);
+        }
+        catch(Exception e){
+        	penSize = 1;
+        }
+        g.setStroke(new BasicStroke(penSize));
         g.drawLine(x1, y1, x2, y2);
         
         // IMPORTANT!  every time we draw on the internal drawing buffer, we
@@ -128,16 +187,27 @@ public class Canvas extends JPanel {
     /*
      * Add the mouse listener that supports the user's freehand drawing.
      */
-    private void addDrawingController() {
+    private void addDrawingController() 
+    {
         DrawingController controller = new DrawingController();
         addMouseListener(controller);
         addMouseMotionListener(controller);
+    }
+    /*
+     * Add the listener that is used for freehand erasing. 
+     */
+    private void addEraserController()
+    {
+    	EraserController eraser = new EraserController();
+    	addMouseListener(eraser);
+    	addMouseMotionListener(eraser);
     }
     
     /*
      * DrawingController handles the user's freehand drawing.
      */
-    private class DrawingController implements MouseListener, MouseMotionListener {
+    private class DrawingController implements MouseListener, MouseMotionListener 
+    {
         // store the coordinates of the last mouse event, so we can
         // draw a line segment from that last point to the point of the next mouse event.
         private int lastX, lastY; 
@@ -145,21 +215,29 @@ public class Canvas extends JPanel {
         /*
          * When mouse button is pressed down, start drawing.
          */
-        public void mousePressed(MouseEvent e) {
+        public void mousePressed(MouseEvent e) 
+        {
+        	if(!eraserMode)
+        	{
             lastX = e.getX();
             lastY = e.getY();
+        	}
         }
 
         /*
          * When mouse moves while a button is pressed down,
          * draw a line segment.
          */
-        public void mouseDragged(MouseEvent e) {
+        public void mouseDragged(MouseEvent e) 
+        {
+        	if(!eraserMode)
+        	{
             int x = e.getX();
             int y = e.getY();
             drawLineSegment(lastX, lastY, x, y);
             lastX = x;
             lastY = y;
+        	}
         }
 
         // Ignore all these other mouse events.
@@ -170,7 +248,147 @@ public class Canvas extends JPanel {
         public void mouseExited(MouseEvent e) { }
     }
     
+    public static JToggleButton DrawOrErase = new JToggleButton("Currently In Eraser Mode?",false);
+    //DrawOrErase.addMouseListener(new eraseButtonListener());
     
+   //Toggles the boolean that controls erasure vs drawing.  
+   private class eraseButtonListener implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+		eraserMode = !(eraserMode);
+	}
+	//Handle the others in case people do weird things with the button, like
+	//dragging outside while still holding, etc. 
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		if (e.MOUSE_PRESSED == 1){eraserMode = !(eraserMode);}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+		eraserMode = !(eraserMode);
+	}
+		
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		eraserMode = !(eraserMode);
+	}
+	   
+   }
+    
+    private void eraserLineSegment(int x1, int y1, int x2, int y2) 
+    {
+        Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
+        g.setColor(Color.WHITE);
+        g.setStroke(new BasicStroke(Integer.parseInt(strokeWidth.getText())));
+        g.drawLine(x1, y1, x2, y2);
+        
+        // IMPORTANT!  every time we draw on the internal drawing buffer, we
+        // have to notify Swing to repaint this component on the screen.
+        this.repaint();
+    }
+    
+   
+    	
+    //Similar to the drawing controller, but with opposing checks. Otherwise,
+    //aside from variable names, identical. For the Eraser Mode button.
+    private class EraserController implements MouseListener, MouseMotionListener
+    {
+    	//Similar to line segment, but draw in large white strokes.
+    	private int startingX, startingY;
+    	public void mousePressed(MouseEvent e){
+    		if(eraserMode)
+    		{
+    		startingX = e.getX();
+    		startingY = e.getY();
+    		}
+    	}
+    	
+    	public void mouseDragged(MouseEvent e){
+    		if(eraserMode)
+    		{
+    		int x = e.getX();
+    		int y = e.getY();
+    		eraserLineSegment(startingX, startingY, x, y);
+    		startingX = x;
+    		startingY = y;
+    		}
+    	}
+    	
+    	public void mouseMoved(MouseEvent e) { }
+        public void mouseClicked(MouseEvent e) { }
+        public void mouseReleased(MouseEvent e) { }
+        public void mouseEntered(MouseEvent e) { }
+        public void mouseExited(MouseEvent e) { }
+    }
+    
+    public class PaletteListener implements ChangeListener{
+
+		@Override
+		public void stateChanged(ChangeEvent colorChosen) {
+			currentPenColor = palette.getColor();
+			
+			
+		}
+    	
+    }
+    
+    public class PalettePopupListener implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent e) 
+		{
+			Color intermediate= palette.showDialog(palette, "Choose Your Color!", currentPenColor);
+			if (intermediate != null)
+			{
+				currentPenColor = intermediate;
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e) {}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+    	
+    }
+    
+  //For the reset button.
+    private class resetListener implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent e) 
+		{
+			fillWithWhite();	
+		}
+		//Ignore the other cases for this button. You hit it, you hit it.
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e) {}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+    	
+    }
     /*
      * Main program. Make a window containing a Canvas.
      */
