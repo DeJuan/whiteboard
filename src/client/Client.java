5634859375144e455df15b1cc06701a4547a38ec
Joe Hanley <jhanley@mit.edu>
2013-12-06 11:43:21 -0500

    more client work

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,5 @@
 package client;
 import java.awt.*;
-import java.awt.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -27,28 +26,65 @@ public class Client {
 		this.socket= new Socket(this.address, this.port);
 		this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.out.print("joinServer");
 	}
 	
-	public void handleResponses(){
+	
+	/*
+	 * 
+	 */
+	public void listen() throws IOException{
+		for (String line =in.readLine(); line!=null; line=in.readLine()) {
+            handleResponses(line);
+		}
+	}
+	/*
+	 * handleResponses is called on incoming strings from the server. It tokenizes the given string,
+	 * interprets the tokens, and then calls the appropriate methods to do what the string asks.
+	 * @param response - A response from the server, properly formatted per the protocol.
+	 */
+	public void handleResponses(String response){
+		String[] tokens = response.split(" ");
+		if (tokens[0].equals("brushstroke")){
+			Brushstroke newStroke = new Brushstroke(tokens[1]);
+			ourCanvas.drawLineSegment(newStroke);
+		}
 		
+		else if (tokens[0].equals("boardList")){
+			//Display all the boards to the user.
+			String[] boardList = Arrays.copyOfRange(tokens, 1, tokens.length);
+			//TODO: do something with boardList so that the GUI can display available boards.
+		}
+		else{
+			throw new RuntimeException("Recieved an improperly formatted string");
+		}
 	}
 	
 	/*
-	 * interpretString takes a string represent one line segment drawn,
-	 * and calls the appropriate methods to draw that line.
-	 * @param command - Of the form startx,starty,endx,endy,rgbcolor,width
+	 * send will send the given string to the server.
+	 * @param request - a properly formatted request string, as per the protocol.
 	 */
-	public void interpretString(String command){
-		
-		Brushstroke ourStroke = new Brushstroke(command);
-		ourCanvas.drawLineSegment(ourStroke);
+	public void send(String request){
+		this.out.print(request);
 	}
 	
-	public void main() throws IOException{
-		Client client = new Client("blah" , 4444);
-		int rgb = Color.RED.getRGB();
-		client.interpretString("407,271,414,278," + rgb + ",5");
+	/*
+	 * join takes a boardID and sends a properly formatted joinBoard request to the server
+	 */
+	public void join(String boardID){
+		String request = "joinBoard " + boardID;
+		this.send(request);
 	}
+	
+	public void exit(){
+		this.send("exitBoard");
+		//TODO: deal with switching the socket
+	}
+	public void main() throws IOException{
+		//TODO: replace client arguments with the aruguments given by the user.
+		Client client = new Client("blah" , 4444);
+	}
+	
 	
 	
 }
